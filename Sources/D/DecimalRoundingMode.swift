c7adea78fe101f31e003a3315114be987f8c4f3d
Diana Ma <diana@rarestype.com>
2025-10-28 05:07:29 +0000

    better inlining boundary

--- Sources/D/DecimalRoundingMode.swift
+++ Sources/D/DecimalRoundingMode.swift
@@ -1,24 +1,22 @@
-extension Decimal {
-    /// Defines rounding behavior when creating a `Decimal` from a fraction.
-    @frozen public enum FractionalRounding {
-        /// Rounds towards zero (truncation).
-        ///
-        /// - `1.239` (s=3) -> `1.23`
-        /// - `-1.239` (s=3) -> `-1.23`
-        case toZero
+/// Defines rounding behavior when creating a `Decimal` from a fraction.
+@frozen public enum DecimalRoundingMode {
+    /// Rounds towards zero (truncation).
+    ///
+    /// - `1.239` (s=3) -> `1.23`
+    /// - `-1.239` (s=3) -> `-1.23`
+    case toZero
 
-        /// Rounds away from zero if any fractional part exists.
-        ///
-        /// - `1.231` (s=3) -> `1.24`
-        /// - `-1.231` (s=3) -> `-1.24`
-        /// - `1.230` (s=3) -> `1.23`
-        case awayFromZero
+    /// Rounds away from zero if any fractional part exists.
+    ///
+    /// - `1.231` (s=3) -> `1.24`
+    /// - `-1.231` (s=3) -> `-1.24`
+    /// - `1.230` (s=3) -> `1.23`
+    case awayFromZero
 
-        /// Rounds to the nearest value; halves round away from zero.
-        ///
-        /// - `1.235` (s=3) -> `1.24`
-        /// - `1.234` (s=3) -> `1.23`
-        /// - `-1.235` (s=3) -> `-1.24`
-        case nearest
-    }
+    /// Rounds to the nearest value; halves round away from zero.
+    ///
+    /// - `1.235` (s=3) -> `1.24`
+    /// - `1.234` (s=3) -> `1.23`
+    /// - `-1.235` (s=3) -> `-1.24`
+    case nearest
 }
